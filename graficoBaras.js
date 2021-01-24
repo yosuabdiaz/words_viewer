6d9d6eed822e0e7abd0f8edd00aeb3f7df922113
jgonzalezPernix <jgonzalez@pernixlabs.com>
2021-01-23 21:35:07 -0600

    Agregar circulo

--- graficoBaras.js
+++ graficoBaras.js
@@ -1,3 +1,4 @@
+//let angles = [30, 10, 45, 35, 60, 38, 75, 67];
 
 function setup() {
   var w = 900;
@@ -9,95 +10,131 @@ function setup() {
   canvas.position(500,200);
 }
 
+function dotLine(xpos, calificacion)
+{ 
+  var dotPos = 450;
+   // Make the points 10 pixels in size
+
+  for(var i=0; i <= 85; i++){
+    stroke('green'); // Change the color
+    strokeWeight(2);
+    point(xpos, dotPos);
+    dotPos = dotPos - 5;
+  }
+}
+
+function circulo(diametro) {
+  //var palabras = [];
+  //let lastAngle = 0;
+
+  background(255, 255, 255);
+
+  fill('gray');
+
+  circle(width / 2, height / 2, diametro);
+
+  translate(width/2, height/2);
+
+  for (var i = 0; i < 8; i++) {
+    push();
+    rotate(TWO_PI * i / 8);
+
+    //var size = Math.floor(Math.random() * 15);
+    var size = 10;
+    textSize(21 + size);
+    var palabra = 'word ' + i
+
+    //palabras.push({origen: palabra, destino: 'word ' + i + 1 });
+
+    text(palabra, 200, 30);
+
+    stroke(251, 133, 0);
+    strokeWeight(5);
+    line(0, 0, 100, 108);
+
+    fill(0, 102, 153);
+    
+    pop();
+  }
+}
+
 function draw() {
+  push();
   background(240);
   textFont("LemonMilk");
    {
- fill(51, 153, 250);
-  stroke(211, 211, 211);
-  rect(60, 330, 55, 119);
-  rect(150, 230, 55, 219);
-  rect(240, 290, 55, 159);
-  rect(330, 50, 55, 400);  
-     rect(420, 242, 55, 209);
-     rect(510, 222, 55, 229);
-     rect(600, 10, 55, 440);
-       rect(690, 220, 55, 230);
-   
-     text('Mens salary',280, 500); 
-     fill(51, 153, 250);
-     rect(330,480,30,30);
-}
- textSize (12); 
-    fill(255, 51, 153);
-    text('The Gender Pay Gap',405,20);
+    
+    fill(51, 153, 250);
+    stroke(211, 211, 211);  
+  
 
-  if (mouseIsPressed) {   
-      fill(255, 51, 153);
-   rect(60, 360, 55, 88);
-    rect(150, 300, 55, 149);
-    rect(240, 340, 55, 109);
-      rect(330, 105, 55, 345);
-    rect(420, 295, 55, 155);
-    rect(510, 275, 55, 175);
-    rect(600, 170, 55, 280);
-    rect(690, 300, 55, 150);
-
-       text('Womens salary',430, 500); 
-     fill(255, 51, 153);
-rect( 500,480,30,30);
-    
-fill(255, 255, 255);
-textAlign(CENTER);
-  text('-30.1%', 90, 400);
-    text('-40.4%', 180, 400);
-      text('-50.1%', 270, 400);
-   text('-20.4%', 360, 400);
-   text('-36.5%', 450,400);
-    text('-35.8%', 540,400);
-    text('-60.7%', 630,400);
-     text('-24.8%', 720,400);
-    
-  }
- 
-  line(60, 450, 750, 450)
-  line(60, 450, 60, 10)
-  {
-    
- 
- fill(0, 0, 0);
-  textAlign(CENTER);
-text('Janitorial', 90,470);
-    text('Police', 180,470);
-      text('Construction', 270,470);
-    text('Law', 360,470);
-       text('Real Estate', 450,470);
-     text('Engineering', 540,470);
-    text('Medical', 630,470);
-    text('AVERAGE', 720,470);
-    
-    fill (0,0,0);
-text(0,50,449);
-text(20,50,369);
-  text(40,50,289);
-   text(60,50,199);
-    text(80,50,109);
-    text(100,50,20);
-    
-    text('I',10, 150);
-    text('N',10, 170);
-    text('T',10, 200);
-    text('H',10, 220);
-    text('0',10, 240);
-     text('U',10, 260)
-      text('S',10, 280);
-    text('A',10, 300);
-     text('N',10, 320);
-      text('D',10, 340);
-     text('S',10, 360);
-    textSize(10);
-    
+    dotLine(100, 2);
+    dotLine(140, 3);
+    dotLine(200, 4);
+    dotLine(260, 1);
+    dotLine(320, 5);
+    dotLine(380, 4);
+    dotLine(440, 3);
+    dotLine(500, 1);
+    dotLine(560, 5);
+    dotLine(620, 4);
+    dotLine(680, 3);
+    dotLine(740, 1);
+  
   }
 
+  pop();
+
+  textSize (12); 
+  fill(1, 19, 28);
+  text('Amazon Sentiment Analysis',405,20);
+  
+
+  if (mouseIsPressed) {
+    circulo(300);
+  }
+
+  else{
+    line(60, 450, 750, 450)
+    line(60, 450, 60, 10)
+    {
+      fill(0, 0, 0);
+      textAlign(CENTER);
+      text('Enero', 90,470);
+      text('Febrero', 150,470);
+      text('Marzo', 210,470);
+      text('Abril', 270,470);
+      text('Mayo', 330,470);
+      text('Junio', 390,470);
+      text('Julio', 450,470);
+      text('Agosto', 510,470);
+      text('Setiembre', 570,470);
+      text('Octubre', 630,470);
+      text('Noviembre', 690,470);
+      text('Diciembre', 750,470);
+        
+      fill (0,0,0);
+      text(0,50,449);
+      text(1,50,369);
+      text(2,50,289);
+      text(3,50,199);
+      text(4,50,109);
+      text(5,50,20);
+        
+      text('C',10, 150);
+      text('A',10, 170);
+      text('L',10, 200);
+      text('I',10, 220);
+      text('F',10, 240);
+      text('I',10, 260)
+      text('C',10, 280);
+      text('A',10, 300);
+      text('C',10, 320);
+      text('I',10, 340);
+      text('O',10, 360);
+      text('N',10, 380);
+      textSize(10);  
+    }
+  }
 }
 
